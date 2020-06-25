80b16ffaed43215c06a377e7183b3ad4b5f668fe
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-25 10:30:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/show', to: 'secrets#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
+  post '/destroy', to: 'sessions#destroy'
 end
