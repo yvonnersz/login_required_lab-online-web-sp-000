fd9aead79c1c446f79cebd52e1398a7f9dc9c1a0
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-25 10:15:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/show', to: 'secrets#show'
-  get '/sessions/new', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 end
